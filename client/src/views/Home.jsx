61ced75f83c9f4b6185c692c11d1db3f4788a83b
arsato <arsatoji@gmail.com>
2023-11-17 13:18:09 -0300

    Buenos dias a todos

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <main className="">
       <div className="container mx-auto">
-        <h2 className="flex justify-center text-5xl font-bold">Buenos días</h2>
+        <h2 className="flex justify-center text-5xl font-bold">Buenos días a todos</h2>
       </div>
     </main>
   )
