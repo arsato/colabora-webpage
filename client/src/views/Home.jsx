8204cb4c4c378cf671fc95271aef3697fe049a3e
arsato <arsatoji@gmail.com>
2023-11-17 13:04:01 -0300

    Change Home -> Homepage

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <main className="">
       <div className="container mx-auto">
-        <h2 className="flex justify-center text-5xl font-bold">Home</h2>
+        <h2 className="flex justify-center text-5xl font-bold">Homepage</h2>
       </div>
     </main>
   )
