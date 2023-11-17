d0a9fadc8f65c26bf7d0e079a3d004667a398760
Raul Low <raulou92@gmail.com>
2023-11-17 15:51:36 -0300

    sss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count isssss {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
