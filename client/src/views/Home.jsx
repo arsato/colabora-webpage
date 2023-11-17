251b9dc32d54e8677af6644df5b48a778e09c07f
Raul Low <raulou92@gmail.com>
2023-11-17 13:13:48 -0300

    Changing homepage title

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <main className="">
       <div className="container mx-auto">
-        <h2 className="flex justify-center text-5xl font-bold">Homepage</h2>
+        <h2 className="flex justify-center text-5xl font-bold">Buenos días</h2>
       </div>
     </main>
   )
