bb56c4b753e7dd5cd31753246f5f5fb96d9c8f16
Raul Low <raulou92@gmail.com>
2023-11-17 13:19:45 -0300

    Test

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <main className="">
       <div className="container mx-auto">
-        <h2 className="flex justify-center text-5xl font-bold">Buenos días a todos</h2>
+        <h2 className="flex justify-center text-5xl font-bold">Buenos días a todos, el matinal de Chile</h2>
       </div>
     </main>
   )
